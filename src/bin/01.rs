375b1c53ba34f632c7b3d1b12fdb656c0cea0732
Hani Al Sibai <hanialsibai@gmail.com>
2024-12-01 13:00:05 +0000

    Ignore test

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -67,6 +67,7 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn test_part_one_input() {
         let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
         assert_eq!(result, Some(1941353));
